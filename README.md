39e22353022c3cf476626ff363b89444ee6fd400
kingoftheconnors <Connormcp777@gmail.com>
2019-08-14 22:02:33 -0400

    Added targeting other teleporters and crude teleportation

--- README.md
+++ README.md
@@ -7,4 +7,6 @@ Anything that walks on land (plus Koji's drones) can use teleporters.
 
 ## Installation
 
-Download the zip from Github and extract the contents into your steamapp mod folder (can be found in `C:\Program Files (x86)\Steam\steamapps\common\Wargroove\modpacker_bin`). Drag the mod folder onto your wargroove-mod-packer.exe to install the mod.
+Download the zip from Github and extract the contents into your steamapp mod folder (can be found in `C:\Program Files (x86)\Steam\steamapps\common\Wargroove\modpacker_bin` on windows). Drag the mod folder
+onto your wargroove-mod-packer.exe to install the mod. You can add this mod to any maps you make
+in the custom map editor.
--- assets_src/lua/verbs/teleport.lua
+++ assets_src/lua/verbs/teleport.lua
@@ -4,13 +4,26 @@ local Verb = require "wargroove/verb"
 
 local Teleport = Verb:new()
 
+function Teleport:getTargets(unit, endPos, strParam)
+    return Functional.filter(function (targetPos)
+        return self:canExecuteWithTarget(unit, endPos, targetPos, strParam)
+    end, Wargroove.getTargetsInRange(endPos, 20, nil)) -- GET TARGETS FROM ENTIRE MAP
+end
+
 function Teleport:canExecuteAt(unit, endPos)
     local terrainName = Wargroove.getTerrainNameAt(endPos)
     return terrainName == "teleporter"
 end
 
+function Teleport:canExecuteWithTarget(unit, endPos, targetPos, strParam)
+    local fromTerrainName = Wargroove.getTerrainNameAt(endPos)
+    local toTerrainName = Wargroove.getTerrainNameAt(targetPos)
+    return fromTerrainName == "teleporter" and toTerrainName == "teleporter"
+end
+
 function Teleport:execute(unit, targetPos, strParam, path)
-    unit:setHealth(50, unit.id)
+    unit.pos = { x = targetPos.x, y = targetPos.y }
+    Wargroove.updateUnit(unit)
 end
 
 return Teleport
